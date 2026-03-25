a2da62d1fc087acf4189efcefa20298c31cfaf6a
Janik Altmann <janik.altmann@uzh.ch>
2026-03-25 01:54:52 +0100

    #63 added Join Session and Create Session divs

---
+++ app/play/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Card } from "antd";
+
+const Play: React.FC = () => {
+  return (
+    <div className="play-container">
+      <Card className="play-card" title="Create Session">
+        <p>description</p>
+      </Card>
+      <Card className="play-card" title="Join">
+        <p>description</p>
+      </Card>
+    </div>
+  );
+};
+
+export default Play;
