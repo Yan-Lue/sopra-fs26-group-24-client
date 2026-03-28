e864dbb4f31f593ec2a4cfc5d797d4d3a2376aa4
daniloruggieri <danilo.ruggieri@bluewin.ch>
2026-03-28 13:36:08 +0100

    #29 #30 #88 implemented profile page and adapted style, added checks for guest token to reduce access

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import Navbar from "@/components/Navbar";
-import { Card, Typography } from "antd";
+import { Alert, Card, Typography, message } from "antd";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const { Title, Paragraph } = Typography;
 
@@ -11,9 +12,54 @@ const homeDescription =
 
 const Home: React.FC = () => {
   const router = useRouter();
+  // const [isAuthorized, setIsAuthorized] = useState(false);
+  const [infoMessage, setInfoMessage] = useState<string | null>(null);
+  const [messageApi, contextHolder] = message.useMessage();
+
+  useEffect(() => {
+    // commented out for easier testing, can be enabled later
+    // const token = localStorage.getItem("token");
+    // if (token && !token.startsWith("Guest-")) {
+    //   setIsAuthorized(true);
+    // }
+
+    const redirectMessage = sessionStorage.getItem("redirectMessage");
+    if (redirectMessage) {
+      setInfoMessage(redirectMessage);
+      sessionStorage.removeItem("redirectMessage");
+    }
+  }, []);
+
+  // same as with navbar
+  const readStorageString = (key: string) => {
+    const raw = localStorage.getItem(key);
+    if (!raw) return "";
+    try {
+        const parsed = JSON.parse(raw);
+        return typeof parsed === "string" ? parsed : String(parsed);
+    } catch {
+        return raw;
+    }
+    };
+
+    const handleHistoryCardClick = () => {
+    const token = readStorageString("token");
+    if (token.startsWith("Guest-")) {
+        messageApi.info("Guest users have limited access. Please log in to view history.");
+        return;
+    }
+    router.push("/history");
+    };
+
+//     // prevents rendering/flickering when not logged in 
+//   if (!isAuthorized) {
+//     return null;
+//   }
 
   return (
-    <div className="page-with-nav">
+    <>
+      {contextHolder}
+      <div className="page-with-nav">
         <Navbar />
 
         <div className="home-container">
@@ -21,9 +67,19 @@ const Home: React.FC = () => {
             <Title level={1}>Welcome to UWatch!</Title>
             <Paragraph>{homeDescription}</Paragraph>
         </div>
+        
+        {infoMessage && (
+          <Alert
+            title={infoMessage}
+            type="info"
+            showIcon
+            closable={{closeIcon: true, onClose: () => setInfoMessage(null)}}
+            style={{ marginBottom: 16, backgroundColor: "#4da3ee"}}
+          />
+        )}
 
         <div className="home-actions">
-            <Card hoverable className="home-action-card" onClick={() => router.push("/history")}>
+            <Card hoverable className="home-action-card" onClick={handleHistoryCardClick}>
             <Title>History</Title>
             <Paragraph>See your latest saved results here.</Paragraph>
             </Card>
@@ -35,6 +91,7 @@ const Home: React.FC = () => {
         </div>
         </div>
     </div>
+    </>
     );
 };
 
